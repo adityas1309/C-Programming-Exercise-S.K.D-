cd9f1bf2a3736e583d5a5932354394714d0e8ce2
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-03-18 23:17:56 +0530

    Add files via upload

---
+++ chapter13A/chapter13A5.c
@@ -0,0 +1,24 @@
+//Wrie F and C using hash(#)
+#include<stdio.h>
+int main()
+{
+    printf("######\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#####\n");
+    printf("#\n");
+    printf("#\n");
+    printf("\n");
+    printf("   ######   \n");
+    printf(" ##      ##\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf(" ##      ##\n");
+    printf("   ######   \n");
+
+    return 0;
+}
