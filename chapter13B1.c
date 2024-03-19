107aa5c620a57e06a81c13faf3424ec82b446521
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-03-19 20:35:11 +0530

    1

---
+++ chapter13B1.c
@@ -0,0 +1,36 @@
+//Take total marks and provide Grade
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter total marks: ");
+    scanf("%d",&a);
+    if(a<40)
+    {
+        printf("Fail");
+    }
+    else if(a>=40&&a<=495)
+    {
+        printf("D");
+    }
+    else if(a>=50&&a<=59)
+    {
+        printf("C");
+    }
+    else if(a>=60&&a<=69)
+    {
+        printf("B");
+    }
+    else if(a>=70&&a<=79)
+    {
+        printf("A");
+    }
+    else if(a>=80&&a<=89)
+    {
+        printf("E");
+    }
+    else if(a>=90&&a<=100)
+    {
+        printf("O");
+    }
+}
