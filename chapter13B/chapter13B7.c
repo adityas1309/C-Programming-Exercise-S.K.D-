7bff3804deb8b5e4448f22b325d2eb8beed364fa
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-03-20 22:45:52 +0530

    7

---
+++ chapter13B/chapter13B7.c
@@ -0,0 +1,12 @@
+//Input a neg no, and remove negative sign if front of it
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b;
+    printf("Original value= ");
+    scanf("%d",&a);
+    b=abs(a);
+    printf("Absolute value= %d",b);
+    return 0;
+}
