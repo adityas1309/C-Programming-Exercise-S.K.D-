69f3440b0957d67f4780ac0c0ab327864ed7d758
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-03-24 15:37:18 +0530

    8

---
+++ chapter13B/chapter13B8.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+    int id,unit;
+    char name[100];
+    float amt_chr,charge,sur,net;
+    scanf("%d\n",&id);
+    fgets(name,sizeof(name),stdin);
+    scanf("%d",&unit);
+    if(unit<=199)
+    {
+        charge=1.20;
+        amt_chr=unit*charge;
+    }
+    else if(unit>=200&&unit<400)
+    {
+        charge=1.50;
+        amt_chr=unit*charge;
+    }
+    else if(unit>=400&&unit<600)
+    {
+        charge=1.80;
+        amt_chr=unit*charge;
+    }
+    else if(unit>=600)
+    {
+        charge=2.00;
+        amt_chr=unit*charge;
+    }
+    if(amt_chr<100)
+    {
+        amt_chr=0;
+        printf("Minimum bill should be of Rs.100/-\n");
+    }
+    else if(amt_chr>400)
+    {
+        sur=amt_chr*(15.0/100);
+    }
+    else if(amt_chr>=100&&amt_chr<=400)
+    {
+        sur=0;
+    }
+    net=amt_chr+sur;
+    printf("Customer IDNO: %d\n",id);
+    printf("Customer Name: %s",name);
+    printf("Unit consumed: %d\n",unit);
+    printf("Amount Charges @Rs. %.2f per unit: %.2f\n",charge,amt_chr);
+    printf("Surcharge amount: %.2f\n",sur);
+    printf("Net amount paid by the customer: %.2f",net);
+}
