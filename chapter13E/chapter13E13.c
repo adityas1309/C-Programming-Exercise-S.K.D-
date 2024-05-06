10660de878cf3a17a3c2d15800d17a7c66c482bd
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-05-06 13:20:47 +0530

    initial commit

---
+++ chapter13E/chapter13E13.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int rows, i, j, coefficient = 1;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    for (i = 0; i <= rows; i++) {
+            for (int space = 1; space < rows - i; space++) {
+            printf("  ");
+        }
+
+        for (j = 0; j <= i; j++) {
+            if (j == 0 || i == 0)
+                coefficient = 1;
+            else
+                coefficient = coefficient * (i - j + 1) / j;
+
+            printf(" %d ", coefficient);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ chapter13E/chapter13E14.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    for(int i=1;i<=1;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            printf("  ");
+        }
+        for(int k=1;k<=i;k++)
+        {
+            printf("%c ",64+i);
+        }
+    }
+    printf("\n");
+    for(int i=1;i<=n-1;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            printf("  ");
+        }
+        for(int k=1;k<=i;k++)
+        {
+            printf("%c ",k+64);
+        }
+        for(int m=i+65;m>64;m--)
+        {
+            printf("%c ",m);
+        }
+        printf("\n");
+    }
+}
