1499512d766d5baa98af70073f5c89c4ffbc1f25
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-05-14 12:00:08 +0530

    Update chapter13E13.c

--- chapter13E /chapter13E13.c
+++ chapter13E /chapter13E13.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
-
 int main() {
-    int rows, i, j, coefficient = 1;
-
-    printf("Enter the number of rows: ");
-    scanf("%d", &rows);
-
-    for (i = 0; i <= rows; i++) {
-            for (int space = 1; space < rows - i; space++) {
-            printf("  ");
-        }
-
-        for (j = 0; j <= i; j++) {
-            if (j == 0 || i == 0)
-                coefficient = 1;
-            else
-                coefficient = coefficient * (i - j + 1) / j;
-
-            printf(" %d ", coefficient);
-        }
-        printf("\n");
-    }
-
-    return 0;
+   int rows, coef = 1, space, i, j;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 0; i < rows; i++) {
+      for (space = 1; space <= rows - i; space++)
+         printf("  ");
+      for (j = 0; j <= i; j++) {
+         if (j == 0 || i == 0)
+            coef = 1;
+         else
+            coef = coef * (i - j + 1) / j;
+         printf("%4d", coef);
+      }
+      printf("\n");
+   }
+   return 0;
 }
