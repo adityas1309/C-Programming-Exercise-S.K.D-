e655cb1e0481243d7db936ac0b5ae1f1e46fbe54
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-05-14 12:00:46 +0530

    Update chapter13E14.c

--- chapter13E /chapter13E14.c
+++ chapter13E /chapter13E14.c
@@ -7,29 +7,19 @@ int main()
     for(int i=1;i<=1;i++)
     {
         for(int j=1;j<=n;j++)
-        {
             printf("  ");
-        }
         for(int k=1;k<=i;k++)
-        {
             printf("%c ",64+i);
-        }
     }
     printf("\n");
     for(int i=1;i<=n-1;i++)
     {
         for(int j=1;j<=n-i;j++)
-        {
             printf("  ");
-        }
         for(int k=1;k<=i;k++)
-        {
             printf("%c ",k+64);
-        }
         for(int m=i+65;m>64;m--)
-        {
             printf("%c ",m);
-        }
         printf("\n");
     }
 }
